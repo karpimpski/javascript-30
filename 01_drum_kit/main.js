24525df1d080d001bbe0a1cbf086e59bf2093ec2
Michael <mike.karpinski99@gmail.com>
2016-12-13 04:53:25 +0000

    finished drum project (may refine later)

--- 01_drum_kit/main.js
+++ 01_drum_kit/main.js
@@ -1,9 +1,29 @@
+var boxes = document.querySelectorAll('.box');
+
 window.addEventListener('keypress', function(e){
   var letter = String.fromCharCode(e.which);
   var id = '#' + letter + '_sound';
 
   if(document.querySelector(id)){
-    document.querySelector(id).play();
+    playSound(letter);
   }
-    
 });
+
+for(var i = 0; i < boxes.length; i++){
+ boxes[i].addEventListener('click', function(){
+   playSound(this.querySelector('p').innerHTML);
+ });
+}
+
+function playSound(letter){
+  var sound = document.querySelector('#' + letter + '_sound');
+  var box = document.querySelector('#' + letter + '_box');
+  sound.currentTime = 0;
+  box.style.transform = 'scale(1.1)';
+  box.style.borderColor = '#FFC600';
+  setTimeout(function(){
+    box.style.transform = '';
+    box.style.borderColor = 'black';
+  }, 100);
+  sound.play();
+}
