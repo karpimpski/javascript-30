8643f2f9df62474e97ca2103059b3d660a39e753
Michael <mike.karpinski99@gmail.com>
2016-12-13 17:44:55 +0000

    changed listeners so holding down doesnt make multiple sounds

--- 01_drum_kit/main.js
+++ 01_drum_kit/main.js
@@ -1,24 +1,34 @@
 var boxes = document.querySelectorAll('.box');
 
-window.addEventListener('keypress', function(e){
+window.addEventListener('keydown', function(e){
   if(document.querySelector('#' + e.key + '_sound')){
     playSound(e.key);
   }
 });
 
+window.addEventListener('keyup', function(e){
+  if(document.querySelector('#' + e.key + '_sound')){
+    document.querySelector('#' + e.key + '_box').classList.remove('playing');
+  }
+});
+
 for(var i = 0; i < boxes.length; i++){
- boxes[i].addEventListener('click', function(){
+ boxes[i].addEventListener('mousedown', function(){
    playSound(this.querySelector('p').innerHTML);
+ });
+ 
+ boxes[i].addEventListener('mouseup', function(){
+   this.classList.remove('playing');
  });
 }
 
 function playSound(letter){
   var sound = document.querySelector('#' + letter + '_sound');
   var box = document.querySelector('#' + letter + '_box');
-  sound.currentTime = 0;
-  box.classList.add('playing');
-  setTimeout(function(){
-    box.classList.remove('playing');
-  }, 300);
-  sound.play();
+  if(box.classList.contains('playing') == false){
+    sound.currentTime = 0;
+    box.classList.add('playing');
+    sound.play();
+  }
 }
+
