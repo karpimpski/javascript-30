6f820ec66227d56d115b1759e4c52bf0eeff8753
Michael <mike.karpinski99@gmail.com>
2016-12-17 20:21:47 +0000

    some JS refactoring

--- 03_css_variables/main.js
+++ 03_css_variables/main.js
@@ -1,11 +1,9 @@
-var body = document.querySelector('body');
-var style = body.style;
 addListener('spacing', 'px');
 addListener('blur', 'px');
 addListener('color', '');
 
-function addListener(name, unit = null){
+function addListener(name, unit){
   document.getElementsByName(name)[0].addEventListener('input', function(){
-    style.setProperty('--' + name, document.getElementsByName(name)[0].value + unit);
+    document.querySelector('body').style.setProperty('--' + name, document.getElementsByName(name)[0].value + unit);
   });
 }
