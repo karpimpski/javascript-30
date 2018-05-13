2d338cd8236d836f6133ef39e5c7080d4832337b
Michael Karpinski <mike.karpinski99@gmail.com>
2018-05-13 13:19:02 -0400

    simplified time update logic

--- 02_clock/main.js
+++ 02_clock/main.js
@@ -11,36 +11,29 @@ var secondD = document.querySelector('#second_d');
 var time;
 
 setTime();
-minute.style.transform = `rotate(${time.getMinutes() * 6}deg)`;
-hour.style.transform = `rotate(${(time.getHours() + time.getMinutes()/60) * 30}deg)`;
-
-time.getHours() % 12 == 0 ? hourD.innerHTML = '12' : hourD.innerHTML = time.getHours() % 12;
-minuteD.innerHTML = time.getMinutes();
 setInterval(setTime, 1000);
 
 function setTime(){
     time = new Date();
-    if(time.getSeconds() == 0){
-        newMinute();
-        secondD.innerHTML = '00';
-    }
-    else if(time.getSeconds() < 10){
-        second.style.transform = `rotate(${time.getSeconds() * 6 }deg)`;
-        secondD.innerHTML = '0' + time.getSeconds().toString();   
-    }
-    else{
-        second.style.transform = `rotate(${time.getSeconds() * 6 }deg)`;
-        secondD.innerHTML = time.getSeconds();
-    }
+    rotateHands();
+    updateDigital();
 }
 
-function newMinute(){
-    fullCircle(second);
-    var hours = (time.getHours() + time.getMinutes()/60) * 30;
+function rotateHands() {
+    time.getSeconds() == 0 ? fullCircle(second) : second.style.transform = `rotate(${time.getSeconds() * 6 }deg)`;
     time.getMinutes() == 0 ? fullCircle(minute) : minute.style.transform = `rotate(${time.getMinutes() * 6}deg)`;
-    time.getHours() == 0 ? fullCircle(hour) : hour.style.transform = `rotate(${hours}deg)`;
-    time.getHours() % 12 == 0 ? hourD.innerHTML = '12' : hourD.innerHTML = time.getHours() % 12;
-    time.getMinutes() < 10 ? minuteD.innerHTML = '0' + time.getMinutes().toString() : minuteD.innerHTML = time.getMinutes();
+    time.getHours() == 0 ? fullCircle(hour) : hour.style.transform = `rotate(${(time.getHours() + time.getMinutes()/60) * 30}deg)`;
+}
+
+function updateDigital() {
+    secondD.innerHTML = format(time.getSeconds());
+    minuteD.innerHTML = format(time.getMinutes());
+    hourD.innerHTML = format(time.getHours());
+}
+
+function format(value) {
+    if(value < 10) return "0" + value.toString();
+    return value.toString();
 }
 
 function fullCircle(el){
