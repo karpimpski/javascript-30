dbfbc09590c47311d102daf82356a5c0a4cf342c
Michael Karpinski <mike.karpinski99@gmail.com>
2018-05-13 10:37:36 -0400

    finished incomplete comment

--- 03_css_variables/main.js
+++ 03_css_variables/main.js
@@ -5,7 +5,7 @@ linkCss(document.getElementById('blur_input'), 'blur', 'px');
 linkCss(document.getElementById('color_input'), 'color', '');
 
 /**
- * Links
+ * Links an input element and a CSS variable for live style editing.
  * @param {Object} element - HTML element to link
  * @param {String} cssVariable - name of CSS variable to link
  * @param {String} unit - optional. CSS shorthand for the unit of measurement, such as 'px'
